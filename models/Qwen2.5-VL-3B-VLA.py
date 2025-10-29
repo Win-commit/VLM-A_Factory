b825ccafdb748d03fd37f4a0a70c2a6580ea31c0
Runze Li <1069046223@qq.com>
2025-10-29 11:23:31 +0800

    vlm input transation to vla

--- models/Qwen2.5-VL-3B-VLA.py
+++ models/Qwen2.5-VL-3B-VLA.py
@@ -4,27 +4,27 @@ from typing import Dict,Optional,Union
 
 
 
-class Qwen2_5_VL_3B_VLA():
-    def __init__(self,config:Dict):
+class Qwen2_5_VL_3B_VLA(Qwen2_5_VLForConditionalGeneration):
+    def __init__(self):
         super().__init__()
-        self.model = Qwen2_5_VLForConditionalGeneration.from_pretrained(**config)
 
-    def forward(self, 
-        input_ids: Optional[torch.LongTensor] = None,
-        attention_mask: Optional[torch.Tensor] = None,
-        position_ids: Optional[torch.LongTensor] = None,
-        inputs_embeds: Optional[torch.FloatTensor] = None,
-        labels: Optional[torch.LongTensor] = None,
-        use_cache: Optional[bool] = None,
-        output_attentions: Optional[bool] = None,
-        output_hidden_states: Optional[bool] = None,
-        pixel_values: Optional[torch.Tensor] = None,
-        pixel_values_videos: Optional[torch.FloatTensor] = None,
-        image_grid_thw: Optional[torch.LongTensor] = None,
-        video_grid_thw: Optional[torch.LongTensor] = None,
-        rope_deltas: Optional[torch.LongTensor] = None,
-        cache_position: Optional[torch.LongTensor] = None,
-        second_per_grid_ts: Optional[torch.Tensor] = None,
-        logits_to_keep: Union[int, torch.Tensor] = 0,
-        ):
-        return self.model(input_ids, attention_mask, image_ids, video_ids)
+    def process_vla_input(self,message:str):
+        '''
+        将string的message输入转为token，以及配套的atten mask 和label
+        并且针对vla-0实现action的随机遮掩
+        '''
+        '''
+        test:
+        text = processor.apply_chat_template(
+    messages, tokenize=False, add_generation_prompt=True
+        )
+    image_inputs, video_inputs = process_vision_info(messages)
+    inputs = processor(
+        text=[text],
+        images=image_inputs,
+        videos=video_inputs,
+        padding=True,
+        return_tensors="pt",
+    )
+    回头看一下这些都是什么
+        '''
